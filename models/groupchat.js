73b7cfff8c5625b590aeed387d3ffae3c05d1963
Bhavya1504 <124488278+Bhavya1504@users.noreply.github.com>
2023-09-06 12:08:15 +0530

    Add files via upload

---
+++ models/groupchat.js
@@ -0,0 +1,25 @@
+const Sequelize=require("sequelize");
+const sequelize=require('../database');
+const groupchat=sequelize.define("groupchat",
+{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+    },
+    groupid:{
+        type:Sequelize.STRING
+    },
+    massage:{
+        type:Sequelize.STRING(5000)
+    },
+    name:{
+        type:Sequelize.STRING
+    },
+    type:{
+        type:Sequelize.STRING
+    }
+}
+)
+module.exports=groupchat
---
+++ models/groups.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../database");
+
+const groups = sequelize.define("groups", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  role: {
+    type: Sequelize.STRING,
+  },
+  memberName: {
+    type: Sequelize.STRING,
+  }
+});
+
+module.exports = groups;
