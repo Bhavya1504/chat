41e06750179eec71f3e5a3b8dabdf9974e0cd366
Bhavya1504 <124488278+Bhavya1504@users.noreply.github.com>
2023-09-06 11:51:56 +0530

    Create userauthentication.js

---
+++ middleware/userauthentication.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const GROUPS = require("../models/groups");
+const USER = require("../models/user");
+async function authenticationoftoken(req, res, next) {
+  try {
+    const token = req.header("token");
+    const user = jwt.verify(token, process.env.TOKEN);
+    const userdetail = await USER.findOne({ where: { id: user.userId } });
+    req.user = userdetail;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "something went wrong" });
+  }
+}
+
+async function authenticationofadmin(req, res, next) {
+  try {
+    const token = req.header("token");
+    const user = jwt.verify(token, process.env.TOKEN);
+    const userdetail = await USER.findOne({ where: { id: user.userId } });
+
+    if (userdetail) {
+      const userdetail2 = await GROUPS.findAll({
+        where: { userId: userdetail.id }
+      });
+
+      userdetail2.forEach((element) => {
+        if (element.group_id == req.body.group_id) {
+          if (element.role == "Admin") {
+            req.user = userdetail;
+            req.group = element;
+            next();
+          } else {
+            return res.status(402).json({ message: "You are not an admin" });
+          }
+        }
+      });
+    } else {
+      return res.status(500).json({ message: "something went wrong" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "something went wrong" });
+  }
+}
+
+module.exports = { authenticationoftoken, authenticationofadmin };
